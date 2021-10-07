cdf426aed43629c280f2fc05f67ef663a90a3602
Ivan <i.kvashnin02@list.ru>
2021-10-07 11:04:06 +0300

    add libs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,16 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
+  extends: ["plugin:vue/essential", "@vue/airbnb"],
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: "babel-eslint"
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    indent: [2, "tab"],
+    "no-tabs": 0,
+  }
 };
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="mx-auto">
+    <DatePicker/>
+    <a-date-picker class="mx-auto" @change="onChange" />
+    <br />
+    <a-month-picker class="mx-auto" placeholder="Select month" @change="onChange" />
+    <br />
+    <a-range-picker class="mx-auto" @change="onChange" />
+    <br />
+    <a-week-picker class="mx-auto" placeholder="Select week" @change="onChange" />
   </div>
 </template>
+<script>
+import { DatePicker } from 'ant-design-vue';
+
+export default {
+  components: {
+    DatePicker,
+  },
+  methods: {
+    onChange(date, dateString) {
+      console.log(date, dateString);
+    },
+  },
+};
+</script>
